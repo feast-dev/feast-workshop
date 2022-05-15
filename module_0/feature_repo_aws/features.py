73b364b6fa620422dcfd13e996df2feb6f6da7a3
Danny Chiao <danny@tecton.ai>
2022-05-15 11:43:11 -0400

    Add new feature view version
    Signed-off-by: Danny Chiao <danny@tecton.ai>

--- module_0/feature_repo_aws/features.py
+++ module_0/feature_repo_aws/features.py
@@ -23,3 +23,19 @@ driver_hourly_stats_view = FeatureView(
     tags={"production": "True"},
     owner="test2@gmail.com",
 )
+
+driver_hourly_stats_view_v2 = FeatureView(
+    name="driver_hourly_stats_v2",
+    description="Hourly features v2",
+    entities=["driver"],
+    ttl=timedelta(seconds=8640000000),
+    schema=[
+        Field(name="conv_rate", dtype=Float32),
+        Field(name="acc_rate", dtype=Float32),
+        Field(name="miles_driven", dtype=Float32),
+    ],
+    online=True,
+    source=driver_stats,
+    tags={"production": "True"},
+    owner="test2@gmail.com",
+)
