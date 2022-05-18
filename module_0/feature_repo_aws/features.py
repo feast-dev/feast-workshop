273f384b1e61e5af2e7cb20d7ffddd0bbf1076b3
Siddhant Mahalle <siddhantmahalle4@gmail.com>
2022-05-19 04:43:07 +0530

    feast test

--- module_0/feature_repo_aws/features.py
+++ module_0/feature_repo_aws/features.py
@@ -21,5 +21,5 @@ driver_hourly_stats_view = FeatureView(
     online=True,
     source=driver_stats,
     tags={"production": "True"},
-    owner="test2@gmail.com",
+    owner="siddhantmahalle@outlook.com",
 )
