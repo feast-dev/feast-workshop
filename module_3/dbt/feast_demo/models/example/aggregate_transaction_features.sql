2040d9cf4a2894fcfc370810e4781fd65d93ec9b
Danny Chiao <danny@tecton.ai>
2022-10-21 17:39:48 -0400

    fix snowflake query
    Signed-off-by: Danny Chiao <danny@tecton.ai>

--- module_3/dbt/feast_demo/models/example/aggregate_transaction_features.sql
+++ module_3/dbt/feast_demo/models/example/aggregate_transaction_features.sql
@@ -1,27 +1,29 @@
 {{ config(materialized='incremental') }}
 
 -- Note: Snowflake does not support time range windows
--- TODO: verify cross join lateral works. 
-SELECT * 
+SELECT *
 FROM
   (SELECT
       USER_ID,
       t.TIMESTAMP as TIMESTAMP,
-      7D_AVG_AMT
-  FROM TRANSACTIONS t CROSS JOIN LATERAL
-     (SELECT 
-        NAMEORIG as USER_ID,
-        AVG(t2.value) as 7D_AVG_AMT
+      "7D_AVG_AMT"
+   FROM TRANSACTIONS t 
+   CROSS JOIN LATERAL 
+    (
+      SELECT 
+        t.NAMEORIG as USER_ID,
+        AVG(AMOUNT) as "7D_AVG_AMT"
       FROM TRANSACTIONS t2
       WHERE t2.NAMEORIG = t.NAMEORIG AND
-            t2.TIMESTAMP >= t.TIMESTAMP - INTERVAL 7 day AND
-            t2.TIMESTAMP <= t.TIMESTAMP
-     ) a
-  {% if is_incremental() %}
+        t2.TIMESTAMP >= t.TIMESTAMP - INTERVAL '6 DAY' AND
+        t2.TIMESTAMP <= t.TIMESTAMP
+    )
+    {% if is_incremental() %}
     -- "-7 days" because we need at least 6 more days of data in order to compute the aggregation.
     WHERE TIMESTAMP > (SELECT DATEADD(day, -7, MAX(TIMESTAMP)::date) FROM {{ this }})
-  {% endif %})
+    {% endif %}
+   )  
 {% if is_incremental() %}
-  -- "-1 day" to account for late arriving data
-  WHERE TIMESTAMP > (SELECT DATEADD(day, -1, MAX(TIMESTAMP)::date) FROM {{ this }})
+-- "-1 day" to account for late arriving data
+WHERE TIMESTAMP > (SELECT DATEADD(day, -1, MAX(TIMESTAMP)::date) FROM {{ this }})
 {% endif %})
