0b613ba71ccd08aaa0db87e5a1e4b19abdca2da5
doxenix <dominikzalewski97@gmail.com>
2023-01-14 19:44:35 +0100

    added new feature

--- module_0/feature_repo_gcp/data_sources.py
+++ module_0/feature_repo_gcp/data_sources.py
@@ -2,7 +2,7 @@ from feast import FileSource
 
 driver_stats = FileSource(
     name="driver_stats_source",
-    path="gs://feast-workshop-danny/driver_stats.parquet",  # TODO: Replace with your bucket
+    path="gs://feast-workshop-feast-workshop/driver_stats.parquet",  # TODO: Replace with your bucket
     timestamp_field="event_timestamp",
     created_timestamp_column="created",
     description="A table describing the stats of a driver based on hourly logs",
--- module_0/feature_repo_gcp/features.py
+++ module_0/feature_repo_gcp/features.py
@@ -7,16 +7,32 @@ from feast import (
 from feast.types import Float32
 
 from data_sources import *
-from entities import *
+from entities import driver
 
 driver_hourly_stats_view = FeatureView(
     name="driver_hourly_stats",
     description="Hourly features",
-    entities=["driver"],
+    entities=[driver],
     ttl=timedelta(seconds=8640000000),
     schema=[
         Field(name="conv_rate", dtype=Float32),
         Field(name="acc_rate", dtype=Float32),
+    ],
+    online=True,
+    source=driver_stats,
+    tags={"production": "True"},
+    owner="test2@gmail.com",
+)
+
+driver_hourly_stats_view_v2 = FeatureView(
+    name="driver_hourly_stats_v2",
+    description="Hourly features v2",
+    entities=[driver],
+    ttl=timedelta(seconds=8640000000),
+    schema=[
+        Field(name="conv_rate", dtype=Float32),
+        Field(name="acc_rate", dtype=Float32),
+        Field(name="miles_driven", dtype=Float32),
     ],
     online=True,
     source=driver_stats,
