e4ab439ba333775effe5941acacdfa283f46a04d
신우현/Growth Intelligence <t1100418@T1100418PM01.local>
2023-01-19 10:16:50 +0900

    Add new feature
    Signed-off-by: ricoshin <ricoshin@gamil.com>

--- module_0/feature_repo_aws/data_sources.py
+++ module_0/feature_repo_aws/data_sources.py
@@ -2,10 +2,10 @@ from feast import FileSource
 
 driver_stats = FileSource(
     name="driver_stats_source",
-    path="s3://feast-workshop-danny/driver_stats.parquet",  # TODO: Replace with your bucket
+    path="s3://feast-workshop-aidenshin/driver_stats.parquet",  # TODO: Replace with your bucket
     s3_endpoint_override="http://s3.us-west-2.amazonaws.com",  # Needed since s3fs defaults to us-east-1
     timestamp_field="event_timestamp",
     created_timestamp_column="created",
     description="A table describing the stats of a driver based on hourly logs",
-    owner="test2@gmail.com",
+    owner="rico.shin@gmail.com",
 )
--- module_0/feature_repo_aws/features.py
+++ module_0/feature_repo_aws/features.py
@@ -12,11 +12,25 @@ from entities import *
 driver_hourly_stats_view = FeatureView(
     name="driver_hourly_stats",
     description="Hourly features",
-    entities=["driver"],
+    entities=[driver],
     ttl=timedelta(seconds=8640000000),
     schema=[
         Field(name="conv_rate", dtype=Float32),
         Field(name="acc_rate", dtype=Float32),
+    ],
+    online=True,
+    source=driver_stats,
+    tags={"production": "True"},
+    owner="test2@gmail.com",
+)
+
+driver_hourly_stats_view = FeatureView(
+    name="driver_hourly_statsV2",
+    description="Hourly features",
+    entities=[driver],
+    ttl=timedelta(seconds=8640000000),
+    schema=[
+        Field(name="conv_rate", dtype=Float32),
     ],
     online=True,
     source=driver_stats,
