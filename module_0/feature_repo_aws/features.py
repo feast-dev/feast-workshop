c20ccc61143abbe3457d9658c98cd014ae6c22e3
신우현/Growth Intelligence <t1100418@T1100418PM01.local>
2023-01-19 10:25:49 +0900

    Remove new feature
    Signed-off-by: ricoshin <ricoshin@gamil.com>

--- module_0/feature_repo_aws/features.py
+++ module_0/feature_repo_aws/features.py
@@ -23,17 +23,3 @@ driver_hourly_stats_view = FeatureView(
     tags={"production": "True"},
     owner="test2@gmail.com",
 )
-
-driver_hourly_stats_view = FeatureView(
-    name="driver_hourly_statsV2",
-    description="Hourly features",
-    entities=[driver],
-    ttl=timedelta(seconds=8640000000),
-    schema=[
-        Field(name="conv_rate", dtype=Float32),
-    ],
-    online=True,
-    source=driver_stats,
-    tags={"production": "True"},
-    owner="test2@gmail.com",
-)
